b12dcd6dc53e48e6ac16f11828250c3b008d5d7d
anurag Sarkar <sarkaranurag.anjankumar2015@vit.ac.in>
2018-09-12 17:37:15 +0530

    Update smart.py

--- smart.py
+++ smart.py
@@ -10,36 +10,9 @@ SERVER_UNIT_ID = 8
 
 c = ModbusClient()
 
-# define modbus server host, port
-c.host(SERVER_HOST)
-c.port(SERVER_PORT)
-c.unit_id(SERVER_UNIT_ID)
 
-while True:
-    # open or reconnect TCP to server
-    if not c.is_open():
-        if not c.open():
-            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
-
-    # if open() is ok, read register (modbus function 0x03)
-    if c.is_open():
-        
-        mydate=datetime.datetime.now()
-
-        print (str(mydate))
-        # read 54 registers at address 0, store result in regs list
-        regs = c.read_input_registers(0,54)
-        # if success change register value to float
-        if regs:
-            abc=[utils.decode_ieee(f) for f in utils.word_list_to_long(regs)]
-        csvwrite=abc
-        l=len(csvwrite)
-        #truncate the float value upto 3 decimal places
-	
-        for i in range (0,l):
-            csvwrite[i]="%0.3f"%csvwrite[i]
-        #Display real time values on Screen
-'''     print("Power KWH: ",abc[1],"KWH")
+def printer():
+	print("Power KWH: ",abc[1],"KWH")
         print("Power KVAH: ",abc[2],"KVAH")
         print("Power KVAr: ",abc[3],"KVArH")
         print("Line Voltages:" )
@@ -73,7 +46,37 @@ while True:
         print("\tReactive Power on B: ",abc[23],"KVAr")
         print("\tApparent Power on R: ",abc[24],"KVA")
         print("\tApparent Power on Y: ",abc[25],"KVA")
-        print("\tApparent Power on B: ",abc[26],"KVA")'''
+        print("\tApparent Power on B: ",abc[26],"KVA")
+	
+
+# define modbus server host, port
+c.host(SERVER_HOST)
+c.port(SERVER_PORT)
+c.unit_id(SERVER_UNIT_ID)
+
+while True:
+    # open or reconnect TCP to server
+    if not c.is_open():
+        if not c.open():
+            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+
+    # if open() is ok, read register (modbus function 0x03)
+    if c.is_open():
+        
+        mydate=datetime.datetime.now()
+
+        print (str(mydate))
+        # read 54 registers at address 0, store result in regs list
+        regs = c.read_input_registers(0,54)
+        # if success change register value to float
+        if regs:
+            abc=[utils.decode_ieee(f) for f in utils.word_list_to_long(regs)]
+        csvwrite=abc
+        l=len(csvwrite)
+        #truncate the float value upto 3 decimal places
+
+        #Display real time values on Screen
+	print(abc)
 	
         # Time and Date Formatting
         #csvstr = datetime.datetime.strftime(mydate, '%Y/%m/%d -- %H:%M:%S')
@@ -85,6 +88,4 @@ while True:
         #   writer = csv.writer(myFile,delimiter=',',quoting=csv.QUOTE_ALL)
         #   writer.writerow(abc)
 
-    # sleep 5s before next polling
-	#print(abc[0])
    time.sleep(0.5)
